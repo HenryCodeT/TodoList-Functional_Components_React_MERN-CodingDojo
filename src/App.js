8611a3d16037b98a586457b31bd8f7b8ebd1a73d
HenryCodeT <120686@unsaac.edu.pe>
2022-03-23 15:12:26 -0500

    add: aplication with CRUD React and checkboxes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import AddToDoList from './components/AddToDoList';
+import Task from './components/Task';
 
 function App() {
+  
+  // //// FIELDS //////////////////////////////////
+  const [toDoList, setToDoList] = useState([
+    // {
+    //   task: "Program To Do List.",
+    //   complete: false 
+    // },
+    // {
+    //   task: "Program e-comerce",
+    //   complete: true 
+    // }
+  ])
+  
+  // //// CREATE ///////////////////////////////////
+  const addNewTask = (newTask) => {
+    setToDoList( (prevToDoList) => [...prevToDoList,newTask]);
+  }
+
+  // //// UPDATE ///////////////////////////////////
+  const handleOnChange = (e,id) =>{
+    const newToDoList = [...toDoList];
+    newToDoList[id].complete = !newToDoList[id].complete;
+    setToDoList(newToDoList);
+
+  }
+  // //// DELETE ////////////////////////////////////
+
+  const handleOnClick = (e,id) => {
+    let filteredToDoList = toDoList.filter((task,index) => index !== id );
+    setToDoList(filteredToDoList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo List</h1>
+      <AddToDoList addNewTask={addNewTask}/>
+      <h2>List of task</h2>
+      {
+        toDoList.map((item,index) => 
+          <Task task = { item } handleOnChange={handleOnChange} key = { index } id={index} handleOnClick={handleOnClick} />
+        )
+      }
     </div>
   );
 }
